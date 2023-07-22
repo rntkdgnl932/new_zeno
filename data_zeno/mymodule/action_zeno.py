51327442d3ba22ced800042cc5a72c0f7c4757b1
coob <koosanghwi@gmail.com>
2023-07-22 20:16:07 +0900

    ver 1.21

--- data_zeno/mymodule/action_zeno.py
+++ data_zeno/mymodule/action_zeno.py
@@ -620,7 +620,19 @@ def clean_screen_action(cla):
         from function import click_pos_2, click_pos_reg, imgs_set_, drag_pos
         print("clean_screen_action")
 
-
+        # 플 내리기
+        full_path = "c:\\my_games\\zenonia\\data_zeno\\imgs\\check\\zenonia_start_ready.PNG"
+        img_array = np.fromfile(full_path, np.uint8)
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        imgs_ = imgs_set_(0, 0, 960, 1030, v_.now_cla, img, 0.8)
+        if imgs_ is not None and imgs_ != False:
+            full_path = "c:\\my_games\\zenonia\\data_zeno\\imgs\\check\\zenonia_title_2.PNG"
+            img_array = np.fromfile(full_path, np.uint8)
+            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+            # 제노는 3클라 고정
+            imgs_ = imgs_set_(0, 50, 960, 1030, "three", img, 0.8)
+            if imgs_ is not None and imgs_ != False:
+                click_pos_reg(imgs_.x - 40, imgs_.y, v_.now_cla)
 
         # 절전
         full_path = "c:\\my_games\\zenonia\\data_zeno\\imgs\\check\\juljun\\juljun_1.PNG"
