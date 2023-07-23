27db5b9fd18bc4c668618aef5c7e37190b20a564
coob <koosanghwi@gmail.com>
2023-07-23 18:19:34 +0900

    ver 1.242

--- data_zeno/mymodule/main_p.py
+++ data_zeno/mymodule/main_p.py
@@ -2822,27 +2822,27 @@ class game_Playing(QThread):
                             full_path = "c:\\my_games\\zenonia\\data_zeno\\imgs\\check\\game_zenonia_title.PNG"
                             img_array = np.fromfile(full_path, np.uint8)
                             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                            imgs_ = imgs_set_(0, 0, 960, 1030, v_.now_cla, img, 0.8)
+                            imgs_ = imgs_set_(0, 0, 960, 1030, v_.now_cla, img, 0.7)
                             if imgs_ is not None and imgs_ != False:
 
                                 full_path = "c:\\my_games\\zenonia\\data_zeno\\imgs\\check\\zenonia_start_ready.PNG"
                                 img_array = np.fromfile(full_path, np.uint8)
                                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                                imgs_ = imgs_set_(0, 0, 960, 1030, v_.now_cla, img, 0.8)
+                                imgs_ = imgs_set_(0, 0, 960, 1030, v_.now_cla, img, 0.7)
                                 if imgs_ is not None and imgs_ != False:
                                     print("매크로를 내려야 실행됨...10초")
                                     for i in range(10):
                                         full_path = "c:\\my_games\\zenonia\\data_zeno\\imgs\\check\\zenonia_start_ready.PNG"
                                         img_array = np.fromfile(full_path, np.uint8)
                                         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                                        imgs_ = imgs_set_(0, 0, 960, 1030, v_.now_cla, img, 0.8)
+                                        imgs_ = imgs_set_(0, 0, 960, 1030, v_.now_cla, img, 0.7)
                                         if imgs_ is not None and imgs_ != False:
                                             if i > 8:
                                                 full_path = "c:\\my_games\\zenonia\\data_zeno\\imgs\\check\\zenonia_title_2.PNG"
                                                 img_array = np.fromfile(full_path, np.uint8)
                                                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
                                                 # 제노는 3클라 고정
-                                                imgs_ = imgs_set_(0, 50, 960, 1030, "three", img, 0.8)
+                                                imgs_ = imgs_set_(0, 50, 960, 1030, "three", img, 0.7)
                                                 if imgs_ is not None and imgs_ != False:
                                                     click_pos_reg(imgs_.x - 40, imgs_.y, v_.now_cla)
                                                 break
