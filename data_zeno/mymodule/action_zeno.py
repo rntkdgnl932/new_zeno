7357fdb7a46091267880911955b775fc95f22f8d
coob <koosanghwi@gmail.com>
2023-07-23 18:21:44 +0900

    ver 1.242

--- data_zeno/mymodule/action_zeno.py
+++ data_zeno/mymodule/action_zeno.py
@@ -1477,7 +1477,7 @@ def get_season_pass(cla):
                             full_path = "c:\\my_games\\zenonia\\data_zeno\\imgs\\tuto_grow\\select\\confirm.PNG"
                             img_array = np.fromfile(full_path, np.uint8)
                             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                            imgs_ = imgs_set_(415, 730, 545, 770, cla, img, 0.85)
+                            imgs_ = imgs_set_(415, 730, 545, 770, cla, img, 0.75)
                             if imgs_ is not None and imgs_ != False:
                                 click_pos_reg(imgs_.x, imgs_.y, cla)
                                 season_pass2_ = True
--- data_zeno/mymodule/test_.py
+++ data_zeno/mymodule/test_.py
@@ -22,16 +22,16 @@ def go_test():
     cla = "three"
     v_.now_cla = cla
 
-    # chago_drag(160, 690, 160, 170, cla)
+    chago_drag(160, 690, 160, 170, cla)
 
-    full_path = "c:\\my_games\\zenonia\\data_zeno\\imgs\\check\\zenonia_start_ready.PNG"
-    img_array = np.fromfile(full_path, np.uint8)
-    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-    imgs_ = imgs_set_(0, 200, 960, 350, v_.now_cla, img, 0.7)
-    if imgs_ is not None and imgs_ != False:
-        print("매크로를 내려야 실행됨...10초", imgs_)
-    else:
-        print("매크로 제목 안 보여")
+    # full_path = "c:\\my_games\\zenonia\\data_zeno\\imgs\\check\\zenonia_start_ready.PNG"
+    # img_array = np.fromfile(full_path, np.uint8)
+    # img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+    # imgs_ = imgs_set_(0, 200, 960, 350, v_.now_cla, img, 0.7)
+    # if imgs_ is not None and imgs_ != False:
+    #     print("매크로를 내려야 실행됨...10초", imgs_)
+    # else:
+    #     print("매크로 제목 안 보여")
     #
     #(50, 370, 320, 730, cla, img, 0.85)
     # full_path = "c:\\my_games\\zenonia\\data_zeno\\imgs\\chango\\guisok.PNG"
