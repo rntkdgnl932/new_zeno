c0f835498df47a66dff4dfa1c3d908b0a3b310c9
coob <koosanghwi@gmail.com>
2023-07-27 21:06:35 +0900

    ver 1.25

--- data_zeno/mymodule/main_p.py
+++ data_zeno/mymodule/main_p.py
@@ -2884,7 +2884,7 @@ class game_Playing(QThread):
                                     full_path = "c:\\my_games\\zenonia\\data_zeno\\imgs\\check\\game_zenonia_title.PNG"
                                     img_array = np.fromfile(full_path, np.uint8)
                                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                                    imgs_ = imgs_set_(0, 0, 960, 1030, v_.now_cla, img, 0.8)
+                                    imgs_ = imgs_set_(0, 0, 960, 1030, v_.now_cla, img, 0.7)
                                     if imgs_ is not None and imgs_ != False:
                                         is_zeno = True
                                         break
