ddbde5ef1141a12d5932df629a519d90a8bd7381
coob <koosanghwi@gmail.com>
2023-08-02 13:03:55 +0900

    ver 1.28

--- data_zeno/mymodule/main_p.py
+++ data_zeno/mymodule/main_p.py
@@ -2822,7 +2822,7 @@ class game_Playing(QThread):
                             full_path = "c:\\my_games\\zenonia\\data_zeno\\imgs\\check\\game_zenonia_title.PNG"
                             img_array = np.fromfile(full_path, np.uint8)
                             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                            imgs_ = imgs_set_(0, 0, 960, 1030, v_.now_cla, img, 0.7)
+                            imgs_ = imgs_set_(0, 0, 960, 1030, v_.now_cla, img, 0.8)
                             if imgs_ is not None and imgs_ != False:
 
                                 full_path = "c:\\my_games\\zenonia\\data_zeno\\imgs\\check\\zenonia_start_ready.PNG"
@@ -2884,10 +2884,11 @@ class game_Playing(QThread):
                                     full_path = "c:\\my_games\\zenonia\\data_zeno\\imgs\\check\\game_zenonia_title.PNG"
                                     img_array = np.fromfile(full_path, np.uint8)
                                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                                    imgs_ = imgs_set_(0, 0, 960, 1030, v_.now_cla, img, 0.7)
+                                    imgs_ = imgs_set_(0, 0, 960, 1030, v_.now_cla, img, 0.8)
                                     if imgs_ is not None and imgs_ != False:
                                         is_zeno = True
                                         break
+                                    time.sleep(1)
                                 if is_zeno == False:
                                     why = "제노 꺼진게 확실하다"
                                     print(why)
