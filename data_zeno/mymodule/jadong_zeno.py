93f49965a74088e1491e1e8bcf95c4f1989bcba3
coob <koosanghwi@gmail.com>
2024-03-07 19:56:06 +0900

    ver 2.81

--- data_zeno/mymodule/jadong_zeno.py
+++ data_zeno/mymodule/jadong_zeno.py
@@ -217,7 +217,7 @@ def map_list_open_ready(cla, where):
                         full_path = "c:\\my_games\\zenonia\\data_zeno\\imgs\\jadong\\midgard\\midgard_in.PNG"
                         img_array = np.fromfile(full_path, np.uint8)
                         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                        imgs_ = imgs_set_(390, 50, 535, 90, cla, img, 0.85)
+                        imgs_ = imgs_set_(20, 100, 930, 1000, cla, img, 0.85)
                         if imgs_ is not None and imgs_ != False:
                             click_pos_reg(imgs_.x, imgs_.y, cla)
                             for i in range(10):
