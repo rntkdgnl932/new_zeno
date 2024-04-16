8bbf89c1ea15e9c1fedd5410dcc218fec17083ce
coob <koosanghwi@gmail.com>
2024-04-16 17:48:46 +0900

    ver 2.99

--- data_zeno/mymodule/event_18.py
+++ data_zeno/mymodule/event_18.py
@@ -26,6 +26,15 @@ def event_stop(cla):
             print("not_again_view", imgs_)
             click_pos_reg(imgs_.x, imgs_.y, cla)
             time.sleep(0.5)
+        else:
+            full_path = "c:\\my_games\\zenonia\\data_zeno\\imgs\\event_18\\today_one.PNG"
+            img_array = np.fromfile(full_path, np.uint8)
+            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+            imgs_ = imgs_set_(220, 650, 480, 750, cla, img, 0.85)
+            if imgs_ is not None and imgs_ != False:
+                print("today_one", imgs_)
+                click_pos_reg(imgs_.x, imgs_.y, cla)
+                time.sleep(0.5)
 
 
     except Exception as e:
