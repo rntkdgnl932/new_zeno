47b53fa77d2406cd517dcc0fc6e7e6d5cf4ad93d
coob <koosanghwi@gmail.com>
2024-04-30 16:10:52 +0900

    ver 3.11

--- data_zeno/mymodule/event_18.py
+++ data_zeno/mymodule/event_18.py
@@ -13,6 +13,7 @@ import variable as v_
 def event_stop(cla):
     import numpy as np
     import cv2
+    import os
     from function import click_pos_reg, click_pos_2, imgs_set_
     from action_zeno import menu_open
     try:
@@ -35,6 +36,27 @@ def event_stop(cla):
                 print("today_one", imgs_)
                 click_pos_reg(imgs_.x, imgs_.y, cla)
                 time.sleep(0.5)
+            else:
+                # 서버 끊김
+                full_path = "c:\\my_games\\zenonia\\data_zeno\\imgs\\event_18\\server_not_connected.PNG"
+                img_array = np.fromfile(full_path, np.uint8)
+                img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                imgs_ = imgs_set_(220, 700, 300, 900, cla, img, 0.85)
+                if imgs_ is not None and imgs_ != False:
+                    print("server_not_connected", imgs_)
+
+                    dir_path = "C:\\my_games\\load\\zenonia"
+                    file_path = dir_path + "\\start.txt"
+                    file_path2 = dir_path + "\\cla.txt"
+                    with open(file_path, "w", encoding='utf-8-sig') as file:
+                        data = 'no'
+                        file.write(str(data))
+                        time.sleep(0.2)
+                    with open(file_path2, "w", encoding='utf-8-sig') as file:
+                        data = v_.now_cla
+                        file.write(str(data))
+                        time.sleep(0.2)
+                    os.execl(sys.executable, sys.executable, *sys.argv)
 
 
     except Exception as e:
